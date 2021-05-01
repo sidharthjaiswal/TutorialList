695d751d2e717ee3168e840c19a65233df1cfdc0
Sidharth Jaiswal <sidhankit@gmail.com>
2021-05-01 13:51:02 +0200

    Initialize Sequelize

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,10 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const db = require("./app/models");
-db.sequelize.sync();
+
+db.sequelize.sync({ force: true }).then(() => {
+    console.log("Drop and re-sync db.");
+});
 
 
 const app = express();
