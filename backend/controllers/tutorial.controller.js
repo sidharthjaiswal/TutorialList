1e5ba1812625ffc7e3f705c388a23c87aa8e7189
Sidharth Jaiswal <sidhankit@gmail.com>
2021-05-01 13:57:04 +0200

    controller for get all tutorials

--- backend/controllers/tutorial.controller.js
+++ backend/controllers/tutorial.controller.js
@@ -33,7 +33,22 @@ exports.create = (req, res) => {
 
 // Retrieve all Tutorials from the database.
 exports.findAll = (req, res) => {
+    const title = req.query.title;
+    var condition = title ? {
+        title: {
+            [Op.iLike]: `%${title}%`
+        }
+    } : null;
 
+    Tutorial.findAll({ where: condition })
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while retrieving tutorials."
+            });
+        });
 };
 
 // Find a single Tutorial with an id
