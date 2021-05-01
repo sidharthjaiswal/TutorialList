928519c3129c332799db3a32131bc9623c2852ce
Sidharth Jaiswal <sidhankit@gmail.com>
2021-05-01 13:56:03 +0200

    controller for creation of tutorial

--- backend/controllers/tutorial.controller.js
+++ backend/controllers/tutorial.controller.js
@@ -4,7 +4,31 @@ const Op = db.Sequelize.Op;
 
 // Create and Save a new Tutorial
 exports.create = (req, res) => {
+    // Validate request
+    if (!req.body.title) {
+        res.status(400).send({
+            message: "Content can not be empty!"
+        });
+        return;
+    }
 
+    // Create a Tutorial
+    const tutorial = {
+        title: req.body.title,
+        description: req.body.description,
+        published: req.body.published ? req.body.published : false
+    };
+
+    // Save Tutorial in the database
+    Tutorial.create(tutorial)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while creating the Tutorial."
+            });
+        });
 };
 
 // Retrieve all Tutorials from the database.
