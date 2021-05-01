8a261b7c8e91a895835921d4d56efb01ee29bac1
Sidharth Jaiswal <sidhankit@gmail.com>
2021-05-01 13:59:39 +0200

    controller for deleting all

--- backend/controllers/tutorial.controller.js
+++ backend/controllers/tutorial.controller.js
@@ -116,7 +116,18 @@ exports.delete = (req, res) => {
 };
 // Delete all Tutorials from the database.
 exports.deleteAll = (req, res) => {
-
+    Tutorial.destroy({
+            where: {},
+            truncate: false
+        })
+        .then(nums => {
+            res.send({ message: `${nums} Tutorials were deleted successfully!` });
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while removing all tutorials."
+            });
+        });
 };
 
 // Find all published Tutorials
